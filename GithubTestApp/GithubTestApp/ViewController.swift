313cdb25b60093fcb352108f807a220c5a902760
Paul Deynega <paul.deunega@gmail.com>
2017-06-28 21:36:39 +0300

    Initial commit

---
+++ GithubTestApp/GithubTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubTestApp
+//
+//  Created by Pavlo Deynega on 28.06.17.
+//  Copyright © 2017 Pavlo Deynega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
