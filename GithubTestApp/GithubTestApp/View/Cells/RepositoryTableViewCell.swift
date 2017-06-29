8aff0e2357636d20c8ad1b8418005e019dc59332
Paul Deynega <paul.deunega@gmail.com>
2017-06-29 22:45:37 +0300

    Add class for repository cell

---
+++ GithubTestApp/GithubTestApp/View/Cells/RepositoryTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  RepositoryTableViewCell.swift
+//  GithubTestApp
+//
+//  Created by Pavlo Deynega on 29.06.17.
+//  Copyright © 2017 Pavlo Deynega. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryTableViewCell: UITableViewCell {
+    @IBOutlet weak var repositoryNameLabel: UILabel!
+    @IBOutlet weak var repositoryDescriptionLabel: UILabel!
+    @IBOutlet weak var starredCountLabel: UILabel!
+}
