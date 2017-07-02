45f5d278689c64b97a160ac25bc75b80aeba0861
Paul Deynega <paul.deunega@gmail.com>
2017-07-02 19:56:02 +0300

    Update with CommonError

--- GithubTestApp/GithubTestApp/Managers/APIManager.swift
+++ GithubTestApp/GithubTestApp/Managers/APIManager.swift
@@ -10,18 +10,21 @@ import Foundation
 import Alamofire
 
 class APIManager {
-    func searchRepositories(withPageNumber page: Int, resultsCount count: Int, searchQuery query: String? = "", completion: ((GitHubResponse?, Error?) -> Void)?) {
+    func searchRepositories(withPageNumber page: Int, resultsCount count: Int, searchQuery query: String? = "", completion: ((GitHubResponse?, CommonError?) -> Void)?) {
         let query = query ?? ""
         let requestString = "https://api.github.com/search/repositories?page=\(page)&per_page=\(count)&q=\(query)&sort=stars&order=desc"
         _ = Alamofire.request(requestString, method: .get).responseObject { (response: DataResponse<GitHubResponse>) in
-            completion?(response.result.value, response.result.error)
+            let commonError = CommonError(withError: response.result.error)
+            completion?(response.result.value, commonError)
         }
     }
 
     func cancelAllTasks(withCompletion completion: (() -> Void)?) {
         Alamofire.SessionManager.default.session.getAllTasks { (tasks) in
             _ = tasks.map({ $0.cancel() })
-            completion?()
+            DispatchQueue.main.async {
+                completion?()
+            }
         }
     }
 }
