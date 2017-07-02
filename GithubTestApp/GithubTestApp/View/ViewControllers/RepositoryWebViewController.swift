4d4c7e709a729d4f375cbbd5d32d664453393adc
Paul Deynega <paul.deunega@gmail.com>
2017-07-02 20:27:45 +0300

    Got rid of UIWebView and used WKWebView

--- GithubTestApp/GithubTestApp/View/ViewControllers/RepositoryWebViewController.swift
+++ GithubTestApp/GithubTestApp/View/ViewControllers/RepositoryWebViewController.swift
@@ -7,29 +7,45 @@
 //
 
 import UIKit
+import WebKit
 
 class RepositoryWebViewController: UIViewController {
     var linkToOpen: String?
+    var dismissalCompletion: (() -> Void)?
     
-    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var navigationBar: UINavigationBar!
+   private weak var wkWebView: WKWebView!
     
     // MARK: Initialization
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let webView = WKWebView(frame: CGRect.zero)
+        wkWebView = webView
+        view.addSubview(webView)
+        view.sendSubview(toBack: webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        webView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
+        webView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        webView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 0).isActive = true
+
         if let linkToOpen = linkToOpen, let url = URL(string: linkToOpen) {
             let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 25)
-            webView.loadRequest(request)
+            webView.load(request)
         }
     }
     
     // MARK: Actions
     
     @IBAction func didPressDoneButton(_ sender: Any) {
-        webView.stopLoading()
-        webView.delegate = nil
-        webView.removeFromSuperview()
-        webView = nil
+        wkWebView.uiDelegate = nil
+        wkWebView.navigationDelegate = nil
+        wkWebView.stopLoading()
+        
+        dismissalCompletion?()
+        
         dismiss(animated: true, completion: nil)
     }
 }
